7573480026b28b7bca9b5a33f8c08776ee712fbb
Lalit Baldaniya <lalit.baldaniya92@gmail.com>
2025-11-07 11:21:57 +0530

    prj

---
+++ basicproject/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import PropsExample from './BasicComponent/PropsExample'
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <PropsExample/>
+    <hr />
+     
+    </>
+  )
+}
+
+export default App
